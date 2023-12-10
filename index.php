aaff001e069b733b4b98cf4cb33ef3d65f9d967d
LuVe132003 <vu.le132003@hcmut.edu.vn>
2023-12-10 20:30:24 +0700

    add css sidebar

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ $cosovatchat = $conn->query($sqlShowcosovatchat);
     <title>Quản lí kí túc xá</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
